ed55b078fc4748d98df515477070de298938a5ae
studyztp <studyztp@gmail.com>
2023-11-20 23:32:03 -0800

    scripts used

---
+++ scripts/tpp-config.py
@@ -0,0 +1,146 @@
+# Copyright (c) 2021 The Regents of the University of California
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import argparse
+import m5
+from gem5.utils.requires import requires
+from gem5.utils.override import overrides
+from gem5.components.boards.arm_board import ArmBoard
+from gem5.components.memory import DualChannelDDR4_2400
+from gem5.components.memory import SingleChannelDDR4_2400
+from gem5.components.processors.simple_processor import SimpleProcessor
+from gem5.components.processors.cpu_types import CPUTypes
+from gem5.isas import ISA
+from gem5.simulate.simulator import Simulator
+from gem5.simulate.simulator import ExitEvent
+from gem5.resources.resource import Resource, AbstractResource, DiskImageResource, KernelResource,obtain_resource
+from pathlib import Path
+from utils.NumaArmBoard import ArmDMBoard
+from gem5.components.memory.dram_interfaces.ddr4 import DDR4_2400_8x8
+from utils.RemoteMemory import RemoteChanneledMemory
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--bench",
+    type=str,
+    required=True,
+    help="benchmark",
+)
+
+args = parser.parse_args()
+
+from utils.DMCache import ClassicPL1PL2DMCache
+
+# Here we setup the parameters of the l1 and l2 caches.
+cache_hierarchy = ClassicPL1PL2DMCache(
+    l1d_size="32kB",
+    l1i_size="32kB",
+    l2_size="256kB"
+)
+
+# Memory: Dual Channel DDR4 2400 DRAM device.
+
+local_memory = DualChannelDDR4_2400(size="256MB")
+remote_memory = RemoteChanneledMemory(
+        DDR4_2400_8x8,
+        2,
+        64,
+        size="8GB",
+        remote_offset_latency=750
+)
+
+# Here we setup the processor. We use a simple processor.
+processor = SimpleProcessor(
+    cpu_type=CPUTypes.ATOMIC, isa=ISA.ARM, num_cores=2
+)
+
+board = ArmDMBoard(
+    clk_freq="3GHz",
+    processor=processor,
+    local_memory=local_memory,
+    remote_memory=remote_memory,
+    cache_hierarchy=cache_hierarchy,
+)
+
+cmd = [
+    "echo 'running mount -t sysfs - /sys`;",
+    "mount -t sysfs - /sys;",
+    "echo 'running mount -t proc - /proc`;",
+    "mount -t proc - /proc;",
+    "echo 'running numastat`;",
+    "numastat;",
+    "echo 'cat numa_balancing'",
+    "cat /proc/sys/kernel/numa_balancing;",
+    "echo 'write vmstat to host';",
+    "cat /proc/vmstat > init_vmstat.txt;",
+    "m5 writefile init_vmstat.txt;",
+    f"echo 'run {args.bench}.A.x';",
+    "m5 workbegin;",
+    f"/home/ubuntu/NPB/NPB3.4-OMP/bin/{args.bench}.A.x;",
+    "cat /proc/vmstat > end_vmstat.txt;",
+    "m5 writefile end_vmstat.txt;",
+    "sleep 1;",
+    "m5 exit;"
+]
+
+board.set_kernel_disk_workload(
+    kernel=KernelResource("/scr/studyztp/experiments/dm/disk/kg-5-18-vmlinux"),
+    # kernel=obtain_resource(resource_id="arm64-linux-kernel-5.15.36"),
+    bootloader=obtain_resource(resource_id="arm64-bootloader-foundation"),
+    disk_image=DiskImageResource(
+        "/scr/studyztp/experiments/dm/disk/arm64-hpc-2204.img",
+        root_partition="1"),
+    readfile_contents="".join(cmd),
+    checkpoint=Path("/scr/studyztp/experiments/dm/bootup-cpt")
+)
+
+def workbegin_handler():
+    print("reach first workbegin\nreset stats\n")
+    m5.stats.reset()
+    yield False
+    print("reach second workbegin\ndump and reset stats\n")
+    m5.stats.dump()
+    m5.stats.reset()
+    yield False
+
+def workend_handler():
+    print("reached workend\ndump and reset stats\n")
+    m5.stats.dump()
+    m5.stats.reset()
+    yield False
+    
+
+simulator = Simulator(
+    board=board,
+    on_exit_event = {
+        ExitEvent.WORKBEGIN : workbegin_handler(),
+        ExitEvent.WORKEND : workend_handler()
+    }
+)
+
+simulator.run()
+
+print("Reach the end of script\n")
---
+++ scripts/utils/NumaArmBoard.py
@@ -0,0 +1,333 @@
+# Copyright (c) 2023 The Regents of the University of California
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from m5.objects import (
+    AddrRange,
+    VoltageDomain,
+    SrcClockDomain,
+    Terminal,
+    VncServer,
+    IOXBar,
+    BadAddr,
+    ArmSystem,
+    Port,
+    NoncoherentXBar
+)
+from gem5.components.boards.abstract_board import AbstractBoard
+from m5.objects.RealView import VExpress_GEM5_Base, VExpress_GEM5_Foundation
+from m5.objects.ArmSystem import ArmRelease, ArmDefaultRelease
+from m5.objects.ArmFsWorkload import ArmFsLinux
+
+from m5.util.fdthelper import (
+    Fdt,
+    FdtNode,
+    FdtProperty,
+    FdtPropertyStrings,
+    FdtPropertyWords,
+    FdtState,
+)
+from gem5.components.boards.abstract_system_board import AbstractSystemBoard
+import os
+import m5
+from abc import ABCMeta
+from gem5.components.boards.arm_board import ArmBoard
+from gem5.components.processors.abstract_processor import AbstractProcessor
+from gem5.components.memory.abstract_memory_system import AbstractMemorySystem
+from gem5.components.cachehierarchies.abstract_cache_hierarchy import (
+    AbstractCacheHierarchy,
+)
+from gem5.utils.override import overrides
+
+from typing import List, Sequence, Tuple
+
+
+class ArmDMBoard(ArmBoard):
+    __metaclass__ = ABCMeta
+
+    def __init__(
+        self,
+        clk_freq: str,
+        processor: AbstractProcessor,
+        local_memory: AbstractMemorySystem,
+        cache_hierarchy: AbstractCacheHierarchy,
+        remote_memory: AbstractMemorySystem,
+        platform: VExpress_GEM5_Base = VExpress_GEM5_Foundation(),
+        release: ArmRelease = ArmDefaultRelease(),
+    ) -> None:
+
+        self._localMemory = local_memory
+        self._remoteMemory = remote_memory
+        super().__init__(
+            clk_freq=clk_freq,
+            processor=processor,
+            memory=local_memory,
+            cache_hierarchy=cache_hierarchy,
+            platform=platform,
+            release=release,
+        )
+
+    @overrides(AbstractSystemBoard)
+    def get_memory(self) -> "AbstractMemory":
+        """Get the memory (RAM) connected to the board.
+
+        :returns: The memory system.
+        """
+        raise NotImplementedError
+
+    def get_local_memory(self) -> "AbstractMemory":
+        """Get the memory (RAM) connected to the board.
+        :returns: The local memory system.
+        """
+        return self._localMemory
+
+    def get_remote_memory(self) -> "AbstractMemory":
+        """Get the memory (RAM) connected to the board.
+        :returns: The remote memory system.
+        """
+        return self._remoteMemory
+    
+    @overrides(AbstractSystemBoard)
+    def get_mem_ports(self) -> Sequence[Tuple[AddrRange, Port]]:
+        return self.get_local_memory().get_mem_ports()
+
+    def get_remote_mem_ports(self) -> Sequence[Tuple[AddrRange, Port]]:
+        return self.get_remote_memory().get_mem_ports()
+    
+    @overrides(AbstractSystemBoard)
+    def _setup_memory_ranges(self):
+
+        # the memory has to be setup for both the memory ranges. there is one
+        # local memory range, close to the host machine and the other range is
+        # pure memory, far from the host.
+        local_memory = self.get_local_memory()
+        remote_memory = self.get_remote_memory()
+
+        local_mem_size = local_memory.get_size()
+        remote_mem_size = remote_memory.get_size()
+
+
+        self._local_mem_ranges = [
+            AddrRange(start=0x80000000, size=local_mem_size)
+        ]
+
+        # The remote memory starts where the local memory ends. Therefore it
+        # has to be offset by the local memory's size.
+        self._remote_mem_ranges = [
+            AddrRange(start=0x80000000 + local_mem_size, size=remote_mem_size)
+        ]
+
+        # using a _global_ memory range to keep a track of all the memory
+        # ranges. This is used to generate the dtb for this machine
+        self._global_mem_ranges = []
+        self._global_mem_ranges.append(self._local_mem_ranges[0])
+        self._global_mem_ranges.append(self._remote_mem_ranges[0])
+
+        self.__global_size = local_mem_size
+        self.__global_size += remote_mem_size
+
+        # setting the memory ranges for both of the memory ranges.
+        local_memory.set_memory_range(self._local_mem_ranges)
+        remote_memory.set_memory_range(self._remote_mem_ranges)
+
+    @overrides(ArmBoard)
+    def _setup_board(self) -> None:
+
+        # This board is expected to run full-system simulation.
+        # Loading ArmFsLinux() from `src/arch/arm/ArmFsWorkload.py`
+        self.workload = ArmFsLinux()
+
+        # We are fixing the following variable for the ArmSystem to work. The
+        # security extension is checked while generating the dtb file in
+        # realview. This board does not have security extension enabled.
+        self._have_psci = False
+
+        # highest_el_is_64 is set to True. True if the register width of the
+        # highest implemented exception level is 64 bits.
+        self.highest_el_is_64 = True
+
+        # Setting up the voltage and the clock domain here for the ARM board.
+        # The ArmSystem/RealView expects voltage_domain to be a parameter.
+        # The voltage and the clock frequency are taken from the devices.py
+        # file from configs/example/arm. We set the clock to the same frequency
+        # as the user specified in the config script.
+        self.voltage_domain = VoltageDomain(voltage="1.0V")
+        self.clk_domain = SrcClockDomain(
+            clock=self._clk_freq, voltage_domain=self.voltage_domain
+        )
+
+        # The ARM board supports both Terminal and VncServer.
+        self.terminal = Terminal()
+        self.vncserver = VncServer()
+
+        # Incoherent I/O Bus
+        self.iobus = IOXBar()
+        self.iobus.badaddr_responder = BadAddr()
+        self.iobus.default = self.iobus.badaddr_responder.pio
+
+        # We now need to setup the dma_ports.
+        self._dma_ports = None
+
+        # RealView sets up most of the on-chip and off-chip devices and GIC
+        # for the ARM board. These devices' information is also used to
+        # generate the dtb file. We then connect the I/O devices to the
+        # I/O bus.
+        self._setup_io_devices()
+
+        # Once the realview is setup, we can continue setting up the memory
+        # ranges. ArmBoard's memory can only be setup once realview is
+        # initialized.
+        # memory = self.get_local_memory()
+        # mem_size = memory.get_size()
+
+        # # The following code is taken from configs/example/arm/devices.py. It
+        # # sets up all the memory ranges for the board.
+        # self.mem_ranges = []
+        # success = False
+        # # self.mem_ranges.append(self.get_remote_memory_addr_range())
+        # for mem_range in self.realview._mem_regions:
+        #     size_in_range = min(mem_size, mem_range.size())
+        #     self.mem_ranges.append(
+        #         AddrRange(start=mem_range.start, size=size_in_range)
+        #     )
+
+        #     mem_size -= size_in_range
+        #     if mem_size == 0:
+        #         success = True
+        #         break
+
+        # if success:
+        #     memory.set_memory_range(self.mem_ranges)
+        # else:
+        #     raise ValueError("Memory size too big for platform capabilities")
+
+        # self.mem_ranges.append(self.get_remote_memory_addr_range())
+
+        # The PCI Devices. PCI devices can be added via the `_add_pci_device`
+        # function.
+        self._pci_devices = []
+
+    @overrides(ArmSystem)
+    def generateDeviceTree(self, state):
+        # Generate a device tree root node for the system by creating the root
+        # node and adding the generated subnodes of all children.
+        # When a child needs to add multiple nodes, this is done by also
+        # creating a node called '/' which will then be merged with the
+        # root instead of appended.
+
+        def generateMemNode(numa_node_id, mem_range):
+            node = FdtNode(f"memory@{int(mem_range.start):x}")
+            node.append(FdtPropertyStrings("device_type", ["memory"]))
+            node.append(
+                FdtPropertyWords(
+                    "reg",
+                    state.addrCells(mem_range.start)
+                    + state.sizeCells(mem_range.size()),
+                )
+            )
+            node.append(FdtPropertyWords("numa-node-id", [numa_node_id]))
+            return node
+
+        root = FdtNode("/")
+        root.append(state.addrCellsProperty())
+        root.append(state.sizeCellsProperty())
+
+        # Add memory nodes
+        # for mem_range in self._local_mem_ranges:
+        #     root.append(generateMemNode(0, mem_range))
+        # root.append(generateMemNode(1, self._remote_mem_ranges[0]))
+        for idx, mem_range in enumerate(self._global_mem_ranges):
+            root.append(generateMemNode(idx, mem_range))
+
+        for node in self.recurseDeviceTree(state):
+            # Merge root nodes instead of adding them (for children
+            # that need to add multiple root level nodes)
+            if node.get_name() == root.get_name():
+                root.merge(node)
+            else:
+                root.append(node)
+
+        return root
+
+    @overrides(ArmBoard)
+    def get_default_kernel_args(self) -> List[str]:
+
+        # The default kernel string is taken from the devices.py file.
+        return [
+            "console=ttyAMA0",
+            "lpj=19988480",
+            "norandmaps",
+            "root={root_value}",
+            "rw",
+        ]
+    
+    @overrides(AbstractBoard)
+    def _connect_things(self) -> None:
+        """Connects all the components to the board.
+
+        The order of this board is always:
+
+        1. Connect the memory.
+        2. Connect the cache hierarchy.
+        3. Connect the processor.
+
+        Developers may build upon this assumption when creating components.
+
+        Notes
+        -----
+
+        * The processor is incorporated after the cache hierarchy due to a bug
+        noted here: https://gem5.atlassian.net/browse/GEM5-1113. Until this
+        bug is fixed, this ordering must be maintained.
+        * Once this function is called `_connect_things_called` *must* be set
+        to `True`.
+        """
+
+        if self._connect_things_called:
+            raise Exception(
+                "The `_connect_things` function has already been called."
+            )
+
+        # Incorporate the memory into the motherboard.
+        self.get_local_memory().incorporate_memory(self)
+        self.get_remote_memory().incorporate_memory(self)
+
+        # Incorporate the cache hierarchy for the motherboard.
+        if self.get_cache_hierarchy():
+            self.get_cache_hierarchy().incorporate_cache(self)
+
+        # Incorporate the processor into the motherboard.
+        self.get_processor().incorporate_processor(self)
+
+        self._connect_things_called = True
+
+    @overrides(AbstractBoard)
+    def _post_instantiate(self):
+        """Called to set up anything needed after m5.instantiate"""
+        self.get_processor()._post_instantiate()
+        if self.get_cache_hierarchy():
+            self.get_cache_hierarchy()._post_instantiate()
+        self.get_local_memory()._post_instantiate()
+        self.get_remote_memory()._post_instantiate()
